969f5384eccd47061339324f3079451a125ecf89
Bruno Mayerle Leite <brleite@gmail.com>
2022-05-13 09:30:54 -0300

    Ajustes na inicialização do ngrok

--- scripts/start_ngrok.sh
+++ scripts/start_ngrok.sh
@@ -6,15 +6,19 @@ LOCAL_PORT=${1-8080}
 echo "Start ngrok in background on port [ $LOCAL_PORT ]"
 nohup /snap/bin/ngrok http ${LOCAL_PORT} & > /tmp/start_ngrok.out 2>&1
 
-echo -n "Extracting ngrok public url ."
+echo "Extracting ngrok public url ."
+contador=1
 NGROK_PUBLIC_URL=""
 while [ -z "$NGROK_PUBLIC_URL" ]; do
+  echo "Tentativa: $contador"
+
   # Run 'curl' against ngrok API and extract public (using 'sed' command)
   export NGROK_PUBLIC_URL=$(curl --silent --max-time 10 --connect-timeout 5 \
                             --show-error http://127.0.0.1:4040/api/tunnels | \
                             sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
-  sleep 1
-  echo -n "."
+  sleep 5
+  #echo -n "."
+  contador=$(($contador+1))
 done
 
 echo
